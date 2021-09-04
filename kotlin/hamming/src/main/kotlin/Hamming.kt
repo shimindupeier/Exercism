8c66a048726778abff753f19531f319806a929b4
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-09-04 20:23:41 +0100

    test complete

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,19 @@
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+
+        if (leftStrand.length != rightStrand.length)
+            throw IllegalArgumentException("left and right strands must be of equal length")
+
+        if (leftStrand.isBlank()
+            || leftStrand == rightStrand
+        ) return 0
+
+        val listLS = leftStrand.toList()
+        val listRS = rightStrand.toList()
+        var distance = 0
+        listLS.forEachIndexed { index, c -> if (listRS[index].toString() != c.toString()) distance += 1 }
+
+        return distance
     }
 }
