a7fb38b58e32de1454801d075bee5cb882ee69aa
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-09-05 23:11:58 +0100

    test complete

--- kotlin/raindrops/src/main/kotlin/Raindrops.kt
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -1,6 +1,13 @@
 object Raindrops {
 
     fun convert(n: Int): String {
-        TODO("Implement this function to complete the task")
+        val factorMap = hashMapOf(
+            3 to "Pling",
+            5 to "Plang",
+            7 to "Plong"
+        )
+
+        val rainDropsString: String = factorMap.toSortedMap().map { i -> if (n.rem(i.key) == 0) i.value else ""}.toList().joinToString("")
+        return if (rainDropsString == "") n.toString() else rainDropsString
     }
 }
