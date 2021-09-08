58f0c5af22f4d092c4fac6f314dd2cd4657520f2
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-09-08 23:05:38 +0100

    Implemented

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,10 +1,25 @@
 object ScrabbleScore {
 
     fun scoreLetter(c: Char): Int {
-        TODO("Implement this function to complete the task")
+        val letterMap = mapOf(
+            1 to setOf('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'),
+            2 to setOf('D', 'G'),
+            3 to setOf('B', 'C', 'M', 'P'),
+            4 to setOf('F', 'H', 'V', 'W', 'Y'),
+            5 to setOf('K'),
+            8 to setOf('J', 'X'),
+            10 to setOf('Q','Z')
+        )
+
+        val temp2 = letterMap.filter { v -> v.value.contains(c) }.keys.first()
+        return temp2
     }
 
     fun scoreWord(word: String): Int {
-        TODO("Implement this function to complete the task")
+
+        val wordChar = word.toUpperCase().toList()
+
+        val score = wordChar.sumBy { i -> scoreLetter(i) }
+        return score
     }
 }
