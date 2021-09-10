b090aeda9e3ba48a9e7944e0b69c4932b984a22c
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-09-10 20:29:24 +0100

    added punctuation handling

--- kotlin/pangram/src/main/kotlin/Pangram.kt
+++ kotlin/pangram/src/main/kotlin/Pangram.kt
@@ -1,6 +1,8 @@
 object Pangram {
 
     fun isPangram(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val alphabet = "abcdefghijklmnopqrstuvwxyz"
+        val sortedInput = input.toLowerCase().filter { it.isLetter() }.toSortedSet().joinToString("").trim()
+        return alphabet == sortedInput
     }
 }
