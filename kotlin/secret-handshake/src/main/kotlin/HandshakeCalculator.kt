a9aba26464ef637444acaf4b14bd202159ede0b7
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-09-10 23:25:23 +0100

    added punctuation handling

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,6 +1,19 @@
 object HandshakeCalculator {
     fun calculateHandshake(number: Int): List<Signal> {
+        val binaryNum = Integer.toBinaryString(number).map { it.toString().toInt() }.toList()
+        println("binaryNum: $binaryNum")
+        var signalList: MutableList<Signal> = mutableListOf()
+        binaryNum.reversed().forEachIndexed { index, v ->
+            when (index) {
+                0 -> if (v == 1) signalList.add(Signal.WINK)
+                1 -> if (v == 1) signalList.add(Signal.DOUBLE_BLINK)
+                2 -> if (v == 1) signalList.add(Signal.CLOSE_YOUR_EYES)
+                3 -> if (v == 1) signalList.add(Signal.JUMP)
+                4 -> if (v == 1) signalList = signalList.asReversed()
+            }
+        }
+        
+        return signalList
 
-        return emptyList()
     }
 }
--- kotlin/secret-handshake/src/test/kotlin/HandshakeCalculatorTest.kt
+++ kotlin/secret-handshake/src/test/kotlin/HandshakeCalculatorTest.kt
@@ -11,7 +11,6 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(1))
     }
 
-    @Ignore
     @Test
     fun testThatInput2YieldsADoubleBlink() {
         assertEquals(
@@ -19,7 +18,6 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(2))
     }
 
-    @Ignore
     @Test
     fun testThatInput4YieldsACloseYourEyes() {
         assertEquals(
@@ -27,7 +25,7 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(4))
     }
 
-    @Ignore
+    
     @Test
     fun testThatInput8YieldsAJump() {
         assertEquals(
@@ -35,7 +33,7 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(8))
     }
 
-    @Ignore
+    
     @Test
     fun testAnInputThatYieldsTwoActions() {
         assertEquals(
@@ -43,7 +41,7 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(3))
     }
 
-    @Ignore
+    
     @Test
     fun testAnInputThatYieldsTwoReversedActions() {
         assertEquals(
@@ -51,7 +49,7 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(19))
     }
 
-    @Ignore
+    
     @Test
     fun testReversingASingleActionYieldsTheSameAction() {
         assertEquals(
@@ -59,7 +57,7 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(24))
     }
 
-    @Ignore
+    
     @Test
     fun testReversingNoActionsYieldsNoActions() {
         assertEquals(
@@ -67,7 +65,7 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(16))
     }
 
-    @Ignore
+    
     @Test
     fun testInputThatYieldsAllActions() {
         assertEquals(
@@ -75,7 +73,7 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(15))
     }
 
-    @Ignore
+    
     @Test
     fun testInputThatYieldsAllActionsReversed() {
         assertEquals(
@@ -83,7 +81,7 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(31))
     }
 
-    @Ignore
+    
     @Test
     fun testThatInput0YieldsNoActions() {
         assertEquals(
@@ -91,7 +89,7 @@ class HandshakeCalculatorTest {
             HandshakeCalculator.calculateHandshake(0))
     }
 
-    @Ignore
+    
     @Test
     fun testThatInputWithLower5BitsNotSetYieldsNoActions() {
         assertEquals(
