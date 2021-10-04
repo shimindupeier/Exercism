1bde75caf920683dfef26cea66282c10509c7373
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-10-04 21:06:56 +0100

    complete

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,8 @@
-fun transcribeToRna(dna: String): String = ""
+fun transcribeToRna(dna: String): String {
+    val dnaToRnaMap = mapOf("G" to "C", "C" to "G", "T" to "A", "A" to "U")
+    val dnaList = dna.map { it.toString() }.toList()
+    return if (dna.isEmpty()) ""
+    else {
+        dnaList.joinToString("") { dnaToRnaMap.getValue(it) }
+    }
+}
