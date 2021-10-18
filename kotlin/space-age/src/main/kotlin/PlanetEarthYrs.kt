bcbb2849f32ef906ca29465378ce2e53b809c1df
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-10-18 23:22:28 +0100

    add enum class

---
+++ kotlin/space-age/src/main/kotlin/PlanetEarthYrs.kt
@@ -0,0 +1,10 @@
+enum class PlanetEarthYrs(val yrs: Double) {
+    MERCURY(0.2408467),
+    VENUS(0.61519726),
+    EARTH(365.25),
+    MARS(1.8808158),
+    JUPITER(11.862615),
+    SATURN(29.447498),
+    URANUS(84.016846),
+    NEPTUNE(164.79132)
+}
