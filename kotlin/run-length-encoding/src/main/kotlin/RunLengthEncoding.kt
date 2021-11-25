6abcefd43dbfe01f1248b126cc23b9359fc264a5
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-11-25 19:21:16 +0000

    encode

--- kotlin/run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
+++ kotlin/run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
@@ -1,10 +1,28 @@
 object RunLengthEncoding {
 
     fun encode(input: String): String {
-        TODO("Implement this to complete the task")
+        val strList = input.toList()
+        var first = strList.first()
+        val splitList = mutableListOf<String>()
+        var s = ""
+        for (i in strList.indices) {
+            if (strList[i] == first) {
+                 s += strList[i]
+            } else {
+                splitList.add(s)
+                s = strList[i].toString()
+                first = strList[i]
+            }
+        }
+        splitList.add(s)
+        val output = splitList.joinToString("") { (if (it.length > 1 ) it.length.toString() else "") + it.first() }
+        println(splitList)
+        println(output)
+        return output
     }
 
     fun decode(input: String): String {
         TODO("Implement this to complete the task")
     }
 }
+
