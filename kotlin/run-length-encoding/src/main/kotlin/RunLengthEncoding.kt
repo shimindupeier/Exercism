b196b6f03952e3c37ca98437f531f5bcaa575d46
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-11-26 22:40:15 +0000

    decode

--- kotlin/run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
+++ kotlin/run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
@@ -1,28 +1,45 @@
 object RunLengthEncoding {
 
     fun encode(input: String): String {
-        val strList = input.toList()
-        var first = strList.first()
-        val splitList = mutableListOf<String>()
-        var s = ""
-        for (i in strList.indices) {
-            if (strList[i] == first) {
-                 s += strList[i]
-            } else {
-                splitList.add(s)
-                s = strList[i].toString()
-                first = strList[i]
+        return if (input.isEmpty()) "" else {
+            val strList = input.toList()
+            var first = strList.first()
+            val splitList = mutableListOf<String>()
+            var s = ""
+            for (i in strList.indices) {
+                if (strList[i] == first) {
+                    s += strList[i]
+                } else {
+                    splitList.add(s)
+                    s = strList[i].toString()
+                    first = strList[i]
+                }
             }
+            splitList.add(s)
+            val output = splitList.joinToString("") { (if (it.length > 1) it.length.toString() else "") + it.first() }
+//            println(splitList)
+//            println(output)
+            output
         }
-        splitList.add(s)
-        val output = splitList.joinToString("") { (if (it.length > 1 ) it.length.toString() else "") + it.first() }
-        println(splitList)
-        println(output)
-        return output
     }
 
     fun decode(input: String): String {
-        TODO("Implement this to complete the task")
+        return if (input.isEmpty()) "" else {
+            /*
+            (?<=\D)(?=\d) - matches a position between a non-digit (\D) and a digit (\d)
+            (?<=\d)(?=\D) - matches a position between a digit and a non-digit.
+            */
+            val splitStr = input.split("(?<=\\D)(?=\\d)|(?<=\\D)(?=\\D)".toRegex()).map { it.split("(?<=\\d)(?=\\D)".toRegex()) }
+            println(splitStr)
+            var s = ""
+            splitStr.map {
+                val d = it.first().toIntOrNull()
+                if (d != null) {
+                   for (i in 1..(d.toInt())) s += it.last()
+                } else { s += it.last()}
+            }
+            s
+        }
     }
 }
 
