1334edaa1fd4d0902752fcb618ae1a2d7f8cf0b1
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-11-29 19:41:20 +0000

    refactor map

--- kotlin/run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
+++ kotlin/run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
@@ -6,13 +6,21 @@ object RunLengthEncoding {
             var first = strList.first()
             val splitList = mutableListOf<String>()
             var s = ""
-            for (i in strList.indices) {
+            /*for (i in strList.indices) {
                 if (strList[i] == first) {
                     s += strList[i]
                 } else {
                     splitList.add(s)
                     s = strList[i].toString()
                     first = strList[i]
+                }
+            }*/
+            strList.map { c ->
+                if (c == first) s += c
+                else {
+                    splitList.add(s)
+                    s = c.toString()
+                    first = c
                 }
             }
             splitList.add(s)
@@ -27,14 +35,16 @@ object RunLengthEncoding {
             (?<=\D)(?=\d) - matches a position between a non-digit (\D) and a digit (\d)
             (?<=\d)(?=\D) - matches a position between a digit and a non-digit.
             */
-            val splitStr = input.split("(?<=\\D)(?=\\d)|(?<=\\D)(?=\\D)".toRegex()).map { it.split("(?<=\\d)(?=\\D)".toRegex()) }
-            println(splitStr)
+            val splitStr =
+                input.split("(?<=\\D)(?=\\d)|(?<=\\D)(?=\\D)".toRegex()).map { it.split("(?<=\\d)(?=\\D)".toRegex()) }
             var s = ""
             splitStr.map {
                 val d = it.first().toIntOrNull()
                 if (d != null) {
-                   for (i in 1..(d.toInt())) s += it.last()
-                } else { s += it.last()}
+                    for (i in 1..(d.toInt())) s += it.last()
+                } else {
+                    s += it.last()
+                }
             }
             s
         }
