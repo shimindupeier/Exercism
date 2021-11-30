10afe24053e08139dd15c689e8c773b3be2c4d41
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-11-30 22:39:51 +0000

    test simple major scale

--- kotlin/scale-generator/src/main/kotlin/Scale.kt
+++ kotlin/scale-generator/src/main/kotlin/Scale.kt
@@ -1,11 +1,39 @@
 class Scale(private val tonic: String) {
 
+    private val chromaticScaleSharp = listOf("C","C#","D","D#","E","F","F#","G","G#","A","A#","B","C","C#","D","D#","E","F","F#","G","G#","A","A#","B")
+    private val chromaticScaleFlat = listOf("C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B")
+    private val useSharp = listOf("G","D","A","E","B","F#","C","e","b","f#","c#","g#","d#")
+//    private val useFlat = listOf("F","Bb","Eb","Ab","Db","Gb","d","g","c","f","bb","eb")
+
     fun chromatic(): List<String> {
-        TODO("Implement this function to complete the task")
+//        val startIndex: Int
+//        val endIndex: Int
+
+        return if (tonic in useSharp) {
+            val startIndex = chromaticScaleSharp.indexOf(tonic)
+            val endIndex = startIndex + 12
+            chromaticScaleSharp.subList(startIndex, endIndex)
+        } else {
+            val startIndex = chromaticScaleFlat.indexOf(tonic)
+            val endIndex = startIndex + 12
+            chromaticScaleFlat.subList(startIndex, endIndex)
+        }
     }
 
     fun interval(intervals: String): List<String> {
-        TODO("Implement this function to complete the task")
-    }
+        val intervalList = intervals.split("(?<=\\D)(?=\\D)".toRegex())
+        val scale: MutableList<String> = mutableListOf(tonic)
+        var startIndex = chromaticScaleSharp.indexOf(tonic)
 
+        for (i in intervalList.dropLast(1).indices) {
+            if (intervalList[i] == "M") {
+                startIndex += 2
+                scale.add(chromaticScaleSharp[startIndex])
+            } else if (intervalList[i] == "m") {
+                startIndex += 1
+                scale.add(chromaticScaleSharp[startIndex])
+            }
+        }
+        return scale
+    }
 }
