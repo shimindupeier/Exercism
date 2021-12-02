96af5ce13763d2b067e089d9d4cba55f391dbc16
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-02 21:34:29 +0000

    refactor fun interval chromatic

--- kotlin/scale-generator/src/main/kotlin/Scale.kt
+++ kotlin/scale-generator/src/main/kotlin/Scale.kt
@@ -5,60 +5,42 @@ class Scale(private val tonic: String) {
     private val useSharp = listOf("G","D","A","E","B","F#","C","e","b","f#","c#","g#","d#")
 
     fun chromatic(): List<String> {
+        val startIndex: Int
+        val endIndex: Int
         return if (tonic in useSharp) {
-            val startIndex = chromaticScaleSharp.indexOf(tonic)
-            val endIndex = startIndex + 12
+            startIndex = chromaticScaleSharp.indexOf(tonic)
+            endIndex = startIndex + 12
             chromaticScaleSharp.subList(startIndex, endIndex)
         } else {
-            val startIndex = chromaticScaleFlat.indexOf(tonic)
-            val endIndex = startIndex + 12
+            startIndex = chromaticScaleFlat.indexOf(tonic)
+            endIndex = startIndex + 12
             chromaticScaleFlat.subList(startIndex, endIndex)
         }
     }
 
     fun interval(intervals: String): List<String> {
         val intervalList = intervals.split("(?<=\\D)(?=\\D)".toRegex())
-        val scale: MutableList<String> = mutableListOf()
+        val scale: MutableList<String> = mutableListOf(tonic.replaceFirstChar { it.uppercase() })
         var startIndex: Int
 
         if (tonic in useSharp) {
             startIndex = chromaticScaleSharp.indexOf(tonic.replaceFirstChar { it.uppercase() })
-            scale.add(tonic.replaceFirstChar { it.uppercase() })
-            for (i in intervalList.dropLast(1).indices) {
-                when (intervalList[i]) {
-                    "M" -> startIndex += 2
-                    "m" -> startIndex += 1
-                    "A" -> startIndex += 3
-                }
-                scale.add(chromaticScaleSharp[startIndex])
-//                if (intervalList[i] == "M") {
-//                    startIndex += 2
-//                    scale.add(chromaticScaleSharp[startIndex])
-//                } else if (intervalList[i] == "m") {
-//                    startIndex += 1
-//                    scale.add(chromaticScaleSharp[startIndex])
-//                } else if (intervalList[i] == "A") {
-//                    startIndex += 3
-//                    scale.add(chromaticScaleSharp[startIndex])
-//                }
-            }
+            intervalList.dropLast(1).map {
+                startIndex += scaleGenerator(it)
+                scale.add(chromaticScaleSharp[startIndex])}
         } else {
             startIndex = chromaticScaleFlat.indexOf(tonic.replaceFirstChar { it.uppercase() })
-            scale.add(tonic.replaceFirstChar { it.uppercase() })
-            for (i in intervalList.dropLast(1).indices) {
-                if (intervalList[i] == "M") {
-                    startIndex += 2
-                    scale.add(chromaticScaleFlat[startIndex])
-                } else if (intervalList[i] == "m") {
-                    startIndex += 1
-                    scale.add(chromaticScaleFlat[startIndex])
-                } else if (intervalList[i] == "A") {
-                    startIndex += 3
-                    scale.add(chromaticScaleFlat[startIndex])
-                }
-            }
+            intervalList.dropLast(1).map {
+                startIndex += scaleGenerator(it)
+                scale.add(chromaticScaleFlat[startIndex])}
         }
-
         return scale
+    }
+
+    private fun scaleGenerator(interval: String): Int = when (interval) {
+        "M" -> 2
+        "m" -> 1
+        "A" -> 3
+        else -> 0
     }
 }
