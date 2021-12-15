c6c512910e357da6c16717ee1f632f595d9fbd6a
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-15 22:13:20 +0000

    complete

--- kotlin/perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ kotlin/perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -1,8 +1,25 @@
+import java.lang.RuntimeException
 
 enum class Classification {
     DEFICIENT, PERFECT, ABUNDANT
 }
 
 fun classify(naturalNumber: Int): Classification {
-    return Classification.DEFICIENT
+    val factors = findFactors(naturalNumber)
+    val aliquotSum = factors.sum()
+    return when  {
+        aliquotSum == naturalNumber -> Classification.PERFECT
+        aliquotSum > naturalNumber -> Classification.ABUNDANT
+        aliquotSum < naturalNumber -> Classification.DEFICIENT
+        else -> throw RuntimeException()
+    }
+}
+
+fun findFactors(naturalNumber: Int) : List<Int> {
+    return if (naturalNumber < 1) throw RuntimeException()
+    else {
+        val factors: List<Int> = (1..naturalNumber.div(2))
+            .filter { naturalNumber.mod(it) == 0 }
+        factors
+    }
 }
