a116c9754957c0e7dbeea96e7ee99271bb1b64d6
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-22 17:54:28 +0000

    Test ones()

---
+++ kotlin/yacht/src/main/kotlin/Yacht.kt
@@ -0,0 +1,17 @@
+object Yacht {
+
+    fun solve(category: YachtCategory, vararg dices: Int): Int {
+        return when ( category ) {
+            YachtCategory.YACHT -> {
+                if (dices.distinct().size == 1) 50 else 0
+            }
+            YachtCategory.ONES -> {
+                val output = dices.fold(0) {
+                        acc, i -> if (i == 1) acc + i else acc
+                }
+                output
+            }
+            else -> 0
+        }
+    }
+}
