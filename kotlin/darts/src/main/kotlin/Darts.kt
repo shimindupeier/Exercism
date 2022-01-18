d88f726d60af1fd15c6c68bcf2c6efa9e8fa80ea
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-01-18 22:43:24 +0000

    refactored

--- kotlin/darts/src/main/kotlin/Darts.kt
+++ kotlin/darts/src/main/kotlin/Darts.kt
@@ -1,20 +1,19 @@
+import kotlin.math.hypot
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 object Darts {
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        val xCoord: Double = if (x is Int) x.toDouble() else x.toString().toDouble()
-        val yCoord: Double = if (y is Int) y.toDouble() else y.toString().toDouble()
+    fun score(x: Number, y: Number): Int {
         val xOrigin = 0
         val yOrigin = 0
-        val dotPos = sqrt((xOrigin.minus(xCoord).pow(2.0)
-                        + (yOrigin.minus(yCoord)).pow(2.0)))
+        val dotPos = sqrt((xOrigin.minus(x.toDouble()).pow(2.0)
+                        + (yOrigin.minus(y.toDouble())).pow(2.0)))
 
         return when {
             dotPos <= 1 -> 10
-            (1 < dotPos && dotPos <= 5) -> 5
-            (5 < dotPos && dotPos <= 10) -> 1
+            dotPos <= 5 -> 5
+            dotPos <= 10 -> 1
             else -> 0
         }
     }
