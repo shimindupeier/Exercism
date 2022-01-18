00dc43e4d808a5f48e447f52743ae4f8376f81ca
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-01-18 22:29:17 +0000

    implemented

--- kotlin/darts/src/main/kotlin/Darts.kt
+++ kotlin/darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,23 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 object Darts {
 
     fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+        val xCoord: Double = if (x is Int) x.toDouble() else x.toString().toDouble()
+        val yCoord: Double = if (y is Int) y.toDouble() else y.toString().toDouble()
+        val xOrigin = 0
+        val yOrigin = 0
+        val dotPos = (xOrigin.minus(xCoord).pow(2.0)
+                        + (yOrigin.minus(yCoord)).pow(2.0))
+        val radius = sqrt(dotPos)
+
+        val s = when {
+            radius <= 1 -> 10
+            (1 < radius && radius <= 5) -> 5
+            (5 < radius && radius <= 10) -> 1
+            else -> 0
+        }
+        return s
     }
 }
