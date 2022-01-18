2eb06a54d27a6bad4cb4f7b66375dea15a77fa17
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-01-18 22:32:03 +0000

    refactored

--- kotlin/darts/src/main/kotlin/Darts.kt
+++ kotlin/darts/src/main/kotlin/Darts.kt
@@ -8,16 +8,14 @@ object Darts {
         val yCoord: Double = if (y is Int) y.toDouble() else y.toString().toDouble()
         val xOrigin = 0
         val yOrigin = 0
-        val dotPos = (xOrigin.minus(xCoord).pow(2.0)
-                        + (yOrigin.minus(yCoord)).pow(2.0))
-        val radius = sqrt(dotPos)
+        val dotPos = sqrt((xOrigin.minus(xCoord).pow(2.0)
+                        + (yOrigin.minus(yCoord)).pow(2.0)))
 
-        val s = when {
-            radius <= 1 -> 10
-            (1 < radius && radius <= 5) -> 5
-            (5 < radius && radius <= 10) -> 1
+        return when {
+            dotPos <= 1 -> 10
+            (1 < dotPos && dotPos <= 5) -> 5
+            (5 < dotPos && dotPos <= 10) -> 1
             else -> 0
         }
-        return s
     }
 }
