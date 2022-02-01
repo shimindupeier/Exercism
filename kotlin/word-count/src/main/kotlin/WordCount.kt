768bb1a5929172f3152a3a751085f9a0e8b4fbe7
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-02-01 22:17:44 +0000

    change split to \\s

--- kotlin/word-count/src/main/kotlin/WordCount.kt
+++ kotlin/word-count/src/main/kotlin/WordCount.kt
@@ -2,9 +2,10 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        val regexPunct = Regex("[!\"#\$%&()*+,./:;<=>?@^_`{|}~]|(?<=\\s)'|'(?=\\s)|'(?=\$)|\\n")
-        val removePunct = phrase.lowercase().replace(regexPunct, " ")
-        val justAlphaNum = removePunct.split("\\s".toRegex())
+
+        val regexPunctuation = Regex("[!\"#\$%&()*+,./:;<=>?@^_`{|}~]|(?<=\\s)'|'(?=\\s)|'(?=\$)|\\n")
+        val removePunctuation = phrase.lowercase().replace(regexPunctuation, " ")
+        val justAlphaNum = removePunctuation.split("\\s".toRegex())
 
         return justAlphaNum.filterNot { it.isBlank() }.groupingBy { it }.eachCount()
     }
