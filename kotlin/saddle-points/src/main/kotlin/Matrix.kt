133499d5240f2ea1cfcf99f46883361b90582b51
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-02-07 23:26:52 +0000

    base test case

--- kotlin/saddle-points/src/main/kotlin/Matrix.kt
+++ kotlin/saddle-points/src/main/kotlin/Matrix.kt
@@ -1,1 +1,28 @@
 data class MatrixCoordinate(val row: Int, val col: Int)
+
+class Matrix(saddleList: List<List<Int>>) {
+    val saddlePoints = saddlePoints(saddleList)
+}
+
+fun saddlePoints(sl: List<List<Int>>): Set<MatrixCoordinate> {
+    return if (sl.contains(emptyList())) emptySet()
+    else {
+        var row : Int
+        var col : Int
+        val returnSet : MutableSet<MatrixCoordinate> = mutableSetOf()
+        sl.forEach { list ->
+            val listMaxNumInd: Int = list.indexOf( list.maxOf { it } )
+            val slCol = sl
+            val temp = mutableListOf<Int>()
+            slCol.forEach { row2 ->
+                temp.add(row2[listMaxNumInd])
+            }
+            val colMinNumInd = temp.indexOf(temp.minOf { it })
+            row = list.maxOf { it }
+            col = temp.minOf { it }
+            if (col == row)
+                returnSet.add(MatrixCoordinate(colMinNumInd+1, listMaxNumInd+1))
+        }
+        return returnSet
+    }
+}
