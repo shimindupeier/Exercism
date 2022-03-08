1c57a6dd33b8eba6c77dab1b751236b514ba1083
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-03-08 22:48:24 +0000

    refactor

--- kotlin/saddle-points/src/main/kotlin/Matrix.kt
+++ kotlin/saddle-points/src/main/kotlin/Matrix.kt
@@ -1,28 +1,37 @@
-import kotlin.system.exitProcess
-
 data class MatrixCoordinate(val row: Int, val col: Int)
 
 class Matrix(saddleList: List<List<Int>>) {
-    val saddlePoints = saddlePoints(saddleList)
+
+    val saddlePoints = saddleList.flatMapIndexed { rowIndex, row ->
+        val rowMax = row.maxOfOrNull { it }
+        row.mapIndexedNotNull { colIndex, value ->
+            val colMin = saddleList.minOf { it[colIndex] }
+            if (value == rowMax && value == colMin)
+                MatrixCoordinate(rowIndex + 1, colIndex + 1)
+            else
+                null
+        }
+    }.toSet()
+    
 }
 
-fun saddlePoints(sl: List<List<Int>>): Set<MatrixCoordinate> {
-    return if (sl.contains(emptyList())) emptySet()
+fun saddlePoints(saddleList: List<List<Int>>): Set<MatrixCoordinate> {
+    return if (saddleList.contains(emptyList())) emptySet()
     else {
         val resultSet: MutableSet<MatrixCoordinate> = mutableSetOf()
 
-        sl.forEach  { list ->
-            val maxInt = list.maxOf { it }
-            list.forEachIndexed outer@ { index, value ->
-                if (value == maxInt) {
+        saddleList.forEach { row ->
+            val rowMax = row.maxOf { it }
+            row.forEachIndexed addToCol@{ index, value ->
+                if (value == rowMax) {
                     val colList = mutableListOf<Int>()
-                    sl.forEach { item -> colList.add(item[index]) }
-                    val minInt = colList.minOf { it }
+                    saddleList.forEach { item -> colList.add(item[index]) }
+                    val colMin = colList.minOf { it }
                     colList.forEachIndexed { colInd, colValue ->
-                        if (colValue == minInt && colValue == value) {
+                        if (colValue == colMin && colValue == rowMax) {
                             resultSet.add(MatrixCoordinate(colInd + 1, index + 1))
-                            if (list.toSet().size == 1 && list.size > 1)
-                                return@outer
+                            if (row.toSet().size == 1 && row.size > 1)
+                                return@addToCol
                         }
                     }
                 }
