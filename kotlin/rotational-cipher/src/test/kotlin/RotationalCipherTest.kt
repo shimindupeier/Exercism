8803b1041170444580939a07143ec48b53d961c9
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-04-22 22:20:50 +0100

    all test passed

--- kotlin/rotational-cipher/src/test/kotlin/RotationalCipherTest.kt
+++ kotlin/rotational-cipher/src/test/kotlin/RotationalCipherTest.kt
@@ -40,42 +40,36 @@ class RotationalCipherTest {
         assertEquals("z", cipher.encode("m"))
     }
 
-    @Ignore
     @Test
     fun testRotateLowercaseNBy1SingleWrapAround() {
         val cipher = RotationalCipher(13)
         assertEquals("a", cipher.encode("n"))
     }
 
-    @Ignore
     @Test
     fun testRotateCapitalLettersNoWrapAround() {
         val cipher = RotationalCipher(5)
         assertEquals("TRL", cipher.encode("OMG"))
     }
 
-    @Ignore
     @Test
     fun testSpacesAreUnalteredByRotation() {
         val cipher = RotationalCipher(5)
         assertEquals("T R L", cipher.encode("O M G"))
     }
 
-    @Ignore
     @Test
     fun testNumbersAreUnalteredByRotation() {
         val cipher = RotationalCipher(4)
         assertEquals("Xiwxmrk 1 2 3 xiwxmrk", cipher.encode("Testing 1 2 3 testing"))
     }
 
-    @Ignore
     @Test
     fun testPunctuationIsUnalteredByRotation() {
         val cipher = RotationalCipher(21)
         assertEquals("Gzo'n zvo, Bmviyhv!", cipher.encode("Let's eat, Grandma!"))
     }
 
-    @Ignore
     @Test
     fun testAllLettersRotateCorrectly() {
         val cipher = RotationalCipher(13)
