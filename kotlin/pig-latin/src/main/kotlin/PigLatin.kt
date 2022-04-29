430ce4d5cc6468a75aadc26eafe63f39327e87dd
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-04-29 22:55:53 +0100

    single word implementation

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,17 +1,32 @@
 object PigLatin {
 
     fun translate(phrase: String): String {
-        val phraseList = phrase.toList()
         val vowelList = listOf('a', 'e', 'i', 'o', 'u')
-        val consonantList = ('a' .. 'z').toList()
+        val vowelSoundList = listOf("xr", "yt")
+        val consonantList = ('a'..'z').toList()
             .filterNot { vowelList.contains(it) }
 
-        return when (phraseList.first()) {
-            in vowelList -> phrase + "ay"
-            in consonantList -> {
-                phrase.dropWhile { it in consonantList }
-                    .plus(phrase.takeWhile { it in consonantList })
-                    .plus("ay")
+        return when {
+            phrase.first() in vowelList -> phrase + "ay"
+            phrase.take(2) in vowelSoundList -> phrase + "ay"
+            phrase.first() in consonantList -> {
+                if (phrase.startsWith("qu"))
+                    phrase.drop(2) + phrase.take(2) + "ay"
+                else if (phrase.startsWith("y"))
+                    phrase.drop(1) + phrase.take(1) + "ay"
+                else if (phrase.slice(1 .. 1) == "y")
+                    phrase.drop(1) + phrase.take(1) + "ay"
+                else if (phrase.contains('y'))
+                    phrase.dropWhile { it in consonantList && it != 'y'}
+                        .plus(phrase.takeWhile { it in consonantList && it != 'y' })
+                        .plus("ay")
+                else if (phrase.contains("qu")) {
+                    val indexofU = phrase.indexOf("u")
+                    phrase.substring(indexofU+1, phrase.length) + phrase.substring(0, indexofU+1) + "ay"
+                } else
+                    phrase.dropWhile { it in consonantList }
+                        .plus(phrase.takeWhile { it in consonantList })
+                        .plus("ay")
             }
             else -> ""
         }
