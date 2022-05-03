345c875b89e831c78f5e416d73dd34807307038d
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-05-03 20:53:52 +0100

    rename indexOfU

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -3,32 +3,33 @@ object PigLatin {
     fun translate(phrase: String): String {
         val vowelList = listOf('a', 'e', 'i', 'o', 'u')
         val vowelSoundList = listOf("xr", "yt")
-        val consonantList = ('a'..'z').toList()
-            .filterNot { vowelList.contains(it) }
+        val consonantList = ('a'..'z').toList().filterNot { vowelList.contains(it) }
+        val phraseInList = phrase.split(" ")
 
-        return when {
-            phrase.first() in vowelList -> phrase + "ay"
-            phrase.take(2) in vowelSoundList -> phrase + "ay"
-            phrase.first() in consonantList -> {
-                if (phrase.startsWith("qu"))
-                    phrase.drop(2) + phrase.take(2) + "ay"
-                else if (phrase.startsWith("y"))
-                    phrase.drop(1) + phrase.take(1) + "ay"
-                else if (phrase.slice(1 .. 1) == "y")
-                    phrase.drop(1) + phrase.take(1) + "ay"
-                else if (phrase.contains('y'))
-                    phrase.dropWhile { it in consonantList && it != 'y'}
-                        .plus(phrase.takeWhile { it in consonantList && it != 'y' })
-                        .plus("ay")
-                else if (phrase.contains("qu")) {
-                    val indexOfU = phrase.indexOf("u")
-                    phrase.substring(indexOfU+1, phrase.length) + phrase.substring(0, indexOfU+1) + "ay"
-                } else
-                    phrase.dropWhile { it in consonantList }
-                        .plus(phrase.takeWhile { it in consonantList })
-                        .plus("ay")
+        return phraseInList.joinToString(" ") { word ->
+            when {
+                word.first() in vowelList || word.take(2) in vowelSoundList -> word + "ay"
+                word.first() in consonantList -> {
+                    if (word.startsWith("qu"))
+                        word.drop(2) + word.take(2) + "ay"
+                    else if (word.startsWith("y"))
+                        word.drop(1) + word.take(1) + "ay"
+                    else if (word.slice(1..1) == "y")
+                        word.drop(1) + word.take(1) + "ay"
+                    else if (word.contains('y'))
+                        word.dropWhile { it in consonantList && it != 'y' }
+                            .plus(word.takeWhile { it in consonantList && it != 'y' })
+                            .plus("ay")
+                    else if (word.contains("qu")) {
+                        val indexOfU = word.indexOf("u")
+                        word.substring(indexOfU + 1, word.length) + word.substring(0, indexOfU + 1) + "ay"
+                    } else
+                        word.dropWhile { it in consonantList }
+                            .plus(word.takeWhile { it in consonantList })
+                            .plus("ay")
+                }
+                else -> ""
             }
-            else -> ""
         }
     }
 }
