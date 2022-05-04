3a4a48f39b474993d52d8a79670769f122bcf0ad
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-05-04 22:31:50 +0100

    refactor code

--- kotlin/anagram/src/main/kotlin/Anagram.kt
+++ kotlin/anagram/src/main/kotlin/Anagram.kt
@@ -1,15 +1,14 @@
-class Anagram(val source: String) {
+class Anagram(private val source: String) {
 
     fun match(anagrams: Collection<String>): Set<String> {
-        val filterAnagram = anagrams.filterNot { it.equals(source, true)}
+        val filterAnagram = anagrams.filterNot { it.equals(source, true) }
             .filter { it.length == source.length }
             .filter { it.lowercase().toList().containsAll(source.lowercase().toList()) }
+            .filter { item ->
+                val tmp = item.lowercase().groupBy { it }
+                tmp == source.lowercase().groupBy { it }
+            }
 
-        val sourceGrouping = source.lowercase().groupBy { it }
-        val anagramGrouping = filterAnagram.filter { item ->
-            val tmp = item.lowercase().groupBy { it }
-            tmp == sourceGrouping }
-
-        return anagramGrouping.toSet()
+        return filterAnagram.toSet()
     }
 }
