1e48aa984c5cc116c7d0a8a906df9eb9de1b27f4
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-05-04 20:24:50 +0100

    initial commit

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -7,20 +7,21 @@ object PigLatin {
         val splitPhrase = phrase.split(" ")
 
         return splitPhrase.joinToString(" ") { word ->
+            val phraseEnding = "ay"
             when {
-                word.first() in vowel || word.take(2) in vowelSound -> word + "ay"
+                word.first() in vowel || word.take(2) in vowelSound -> word + phraseEnding
                 word.first() in consonant -> {
                     if (word.contains('y') && word.indexOf('y') != 0)
                         word.dropWhile { it in consonant && it != 'y' }
                             .plus(word.takeWhile { it in consonant && it != 'y' })
-                            .plus("ay")
+                            .plus(phraseEnding)
                     else if (word.contains("qu")) {
                         val indexOfU = word.indexOf("u")
-                        word.substring(indexOfU + 1, word.length) + word.substring(0, indexOfU + 1) + "ay"
+                        word.substring(indexOfU + 1, word.length) + word.substring(0, indexOfU + 1) + phraseEnding
                     } else
                         word.dropWhile { it in consonant }
                             .plus(word.takeWhile { it in consonant })
-                            .plus("ay")
+                            .plus(phraseEnding)
                 }
                 else -> ""
             }
