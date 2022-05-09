39a67a5f78f2a20be38bdc75a0214645d1ce0d7a
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-05-09 12:10:38 +0100

    initial commit

---
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -0,0 +1,26 @@
+class DiamondPrinter {
+
+    fun printToList(letter: Char): List<String> {
+        val diamondInList = mutableListOf<String>()
+        val letterList = ('A'..'Z').toList()
+
+        if (letter == 'A')
+            diamondInList.add("A")
+        else {
+            val numRowsCols = ('A'..letter).count()*2-1 //letterList.indexOf(letter) + 2
+
+            for (i in 0 .. numRowsCols/2) {
+                val rowToLetter = MutableList(numRowsCols) { letterList[i] }
+                diamondInList.add(rowToLetter.joinToString(""))
+            }
+
+            for (i in numRowsCols/2-1 downTo 0) {
+                diamondInList.add(diamondInList[i])
+            }
+            println(diamondInList)
+        }
+
+        return diamondInList
+    }
+
+}
