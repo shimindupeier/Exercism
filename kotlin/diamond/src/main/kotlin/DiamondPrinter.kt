ec1f1405dfcf6f30e9c3e031024ff36dba270179
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-05-09 21:42:11 +0100

    solved

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -8,15 +8,22 @@ class DiamondPrinter {
             diamondInList.add("A")
         else {
             val numRowsCols = ('A'..letter).count()*2-1 //letterList.indexOf(letter) + 2
+            var spacerL = numRowsCols/2
+            var spacerR = numRowsCols/2
 
             for (i in 0 .. numRowsCols/2) {
-                val rowToLetter = MutableList(numRowsCols) { letterList[i] }
+                val rowToLetter = MutableList(numRowsCols) { " " }
+                rowToLetter[spacerL] = letterList[i].toString()
+                rowToLetter[spacerR] = letterList[i].toString()
+                spacerL--
+                spacerR++
                 diamondInList.add(rowToLetter.joinToString(""))
             }
 
             for (i in numRowsCols/2-1 downTo 0) {
                 diamondInList.add(diamondInList[i])
             }
+
             println(diamondInList)
         }
 
