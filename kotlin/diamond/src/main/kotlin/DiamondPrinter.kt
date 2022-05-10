5a313500f3e835c37ff76d395676dd64049031ce
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-05-10 22:25:42 +0100

    refactored

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,25 +1,24 @@
 class DiamondPrinter {
 
     fun printToList(letter: Char): List<String> {
-        val diamondInList = mutableListOf<String>()
-        val letterList = ('A'..'Z').toList()
-        val numRowsCols = ('A'..letter).count() * 2 - 1 //letterList.indexOf(letter) + 2
-        var spacerL = numRowsCols / 2
-        var spacerR = numRowsCols / 2
+        val letterList = ('A'..letter).toList()
+        val diamondDimension = letterList.count() * 2 - 1
+        val diamond = MutableList(diamondDimension) { " " }
+        var spacerL = diamondDimension / 2
+        var spacerR = diamondDimension / 2
+        var reverse = diamondDimension - 1
 
-        for (i in 0..numRowsCols / 2) {
-            val rowToLetter = MutableList(numRowsCols) { " " }
-            rowToLetter[spacerL] = letterList[i].toString()
-            rowToLetter[spacerR] = letterList[i].toString()
+        letterList.mapIndexed { i, c ->
+            val rowOfLetters = MutableList(diamondDimension) { " " }
+            rowOfLetters[spacerL] = c.toString()
+            rowOfLetters[spacerR] = c.toString()
             spacerL--
             spacerR++
-            diamondInList.add(rowToLetter.joinToString(""))
+            diamond[i] = rowOfLetters.joinToString("")
+            diamond[reverse] = rowOfLetters.joinToString("")
+            reverse--
         }
 
-        for (i in numRowsCols / 2 - 1 downTo 0) {
-            diamondInList.add(diamondInList[i])
-        }
-
-        return diamondInList
+        return diamond
     }
 }
