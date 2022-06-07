ef677a7e77ab1e4b3b0887233decbaa7b242ce99
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-06-07 15:42:45 +0100

    refactor fun lyrics

--- kotlin/beer-song/src/main/kotlin/BeerSong.kt
+++ kotlin/beer-song/src/main/kotlin/BeerSong.kt
@@ -1,21 +1,13 @@
-object BeerSong {
-    fun verses(startBottles: Int, takeDown: Int): String {
-        var song = ""
-        for (i in startBottles downTo takeDown) {
-            song += verseLyrics(i, i-1)
-            if (i > takeDown) song += "\n"
-        }
-        return song
-    }
+fun lyrics(bottleCount: Int, bottleLeft: Int): String = when (bottleCount) {
+    in (2..99) -> "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer." +
+            "\nTake one down and pass it around, $bottleLeft bottle${if (bottleLeft > 1) "s" else ""} of beer on the wall.\n"
+    1 -> "$bottleCount bottle of beer on the wall, $bottleCount bottle of beer." +
+            "\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    else -> "No more bottles of beer on the wall, no more bottles of beer." +
+            "\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
 }
 
-fun verseLyrics(bottleCount: Int, bottleLeft: Int): String {
-    return when (bottleCount) {
-        in (2..99) -> "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer." +
-                "\nTake one down and pass it around, $bottleLeft bottle${if (bottleLeft > 1) "s" else ""} of beer on the wall.\n"
-        1 -> "$bottleCount bottle of beer on the wall, $bottleCount bottle of beer." +
-                "\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-        else -> "No more bottles of beer on the wall, no more bottles of beer." +
-                "\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    }
+object BeerSong {
+    fun verses(startBottles: Int, takeDown: Int): String = (startBottles downTo takeDown)
+        .joinToString("\n") { lyrics(it, it - 1) }
 }
