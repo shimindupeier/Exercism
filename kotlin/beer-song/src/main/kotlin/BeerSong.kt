9191fda3b76bc91e814f157bc5fcf30b47356f51
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-06-07 14:23:55 +0100

    combined ln2phrase1 and change function name to songLyrics

--- kotlin/beer-song/src/main/kotlin/BeerSong.kt
+++ kotlin/beer-song/src/main/kotlin/BeerSong.kt
@@ -2,27 +2,19 @@ object BeerSong {
     fun verses(startBottles: Int, takeDown: Int): String {
         var song = ""
         for (i in startBottles downTo takeDown) {
-            song += ln1Phrase1(i) +
-                    "${ln2Phrase1(i)} ${ln2Phrase2(i - 1)}"
+            song += verseLyrics(i) +
+                    " ${ln2Phrase2(i - 1)}"
             if (i > takeDown) song += "\n"
         }
         return song
     }
 }
 
-fun ln1Phrase1(bottleCount: Int): String {
+fun verseLyrics(bottleCount: Int): String {
     return when (bottleCount) {
-        in (2..99) -> "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer.\n"
-        1 -> "$bottleCount bottle of beer on the wall, $bottleCount bottle of beer.\n"
-        else -> "No more bottles of beer on the wall, no more bottles of beer.\n"
-    }
-}
-
-fun ln2Phrase1(bottleCount: Int): String {
-    return when (bottleCount) {
-        in (2..99) ->"Take one down and pass it around,"
-        1 -> "Take it down and pass it around,"
-        else -> "Go to the store and buy some more,"
+        in (2..99) -> "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer.\nTake one down and pass it around,"
+        1 -> "$bottleCount bottle of beer on the wall, $bottleCount bottle of beer.\nTake it down and pass it around,"
+        else -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more,"
     }
 }
 
