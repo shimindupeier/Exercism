98b39a75f2cb55e6017b3f615d99f74d84d38199
Jasmine Dupre <jasmine.dupre@ymail.com>
2022-06-10 20:46:19 +0100

    split line

--- kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
+++ kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
@@ -1,8 +1,20 @@
 class IsbnVerifier {
 
     fun isValid(number: String): Boolean {
-        val numberList = number.split("(?=\\d)(?<=\\d)|-".toRegex())
+        val numberList = number.split("(?=\\d)(?<=\\d)|-|(?=X)(?<=X)".toRegex()).toMutableList()
 
-        return false
+        return if (numberList.size != 10
+            || (numberList.last() != "X" && numberList.last().matches( "[a-zA-Z]".toRegex()))
+            || numberList.dropLast(1).any { it.matches( "[a-zA-Z]".toRegex()) }
+        ) false
+        else {
+            if (numberList.last() == "X") numberList[numberList.lastIndex] = "10"
+            var count = numberList.size
+
+            val tmp = numberList.map { it.toInt() }.fold(0) { sum, i ->
+                sum + i * (count--)
+            }
+            tmp.mod(11) == 0
+        }
     }
 }
